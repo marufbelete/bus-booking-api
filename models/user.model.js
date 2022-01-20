9e5d70f24c9d11c46c0cce10321e524b43735b94
maruf <marufbelete9@gmail.com>
2022-01-20 10:06:04 +0300

    user model done

--- models/user.model.js
+++ models/user.model.js
@@ -1,16 +1,31 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  username: {
+  fullName: {
     type: String,
     trim: true,
     required: true,
   },
+phoneNumber:{
+    type: String,
+    trim: true,
+    required: true,
+},
   password: {
     type: String,
     trim: true,
     required: true,
   },
+  userType:{
+    type: String,
+    enum: ['10','20'],
+    trim: true,
+    required: true,
+  },
+  orgCode:{
+    type: String,
+    trim: true,
+  }
 },
   {
     timestamps: true,

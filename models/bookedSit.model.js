3aec7290868043b7745390e1a7b8b5a95cbbd082
maruf <marufbelete9@gmail.com>
2022-01-20 11:28:14 +0300

    bookedsit model done

---
+++ models/bookedSit.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const BookedSitSchema = new mongoose.Schema({
+passangerInfo:[{
+    passangerName: {
+        type: String,
+        trim: true,
+        required: true,
+          },
+    passangerPhone: {
+        type: String,
+        trim: true,
+        required: true,
+        }
+    }],
+numberOfBookedSit: {
+    type: String,
+    trim: true,
+    required: true,
+        },
+user:{
+    type: Schema.Types.ObjectId, ref: 'user', 
+    required: true,
+}
+},
+  {
+    timestamps: true,
+  },
+);
+
+const BookedSit = mongoose.model("bookedsit", BookedSitSchema);
+
+module.exports = BookedSit;

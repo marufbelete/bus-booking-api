3386309c5a5790913ed02548161b655ae1083311
marufbelete <beletemaruf@gmail.com>
2022-04-18 17:48:07 +0300

    done

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const dashboard = require('./routes/dashboard.route');
 const transfer = require('./routes/scheduletransfer.route');
 
 // const socketio=require("./socket/socketio")
-app.use(cors())
+app.use(cors({ origin: true, credentials: true }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())

c38d3a1021b030c0bd24130391c0868f653e2d2b
unknown <beletemaruf@gmail.com>
2022-07-06 10:44:07 +0300

    final backend for test

--- controllers/organizationUser.controller.js
+++ controllers/organizationUser.controller.js
@@ -232,11 +232,17 @@ exports.loginOrganizationUser = async (req, res, next) => {
       throw error;
     }
     const user_role=user.userRole
+    const fullName=user.firstName+" "+user.lastName
     const token = jwt.sign({ sub: user._id, phone_number: user.phone_number,organization_code:organization_code,user_role:user_role,is_mobileuser:false }, process.env.SECRET);
     return res.status(202).cookie("access_token",token,{
       sameSite:'none',
       path:'/',
-      secure:true}).json({auth:true,token})
+      secure:true}).json({
+        auth:true,
+        user:fullName,
+        role:user_role,
+        token
+      })
   }
   catch(error) {
     next(error)

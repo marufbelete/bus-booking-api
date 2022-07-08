2d048a36a334d2b69ecba22e52e3dc50a314d100
unknown <beletemaruf@gmail.com>
2022-07-08 17:49:01 +0300

    final backend for test

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,10 +1,10 @@
 const jwt = require("jsonwebtoken");
 
 const authenticateJWT = (req, res, next) => {
-  const authHeader =req.cookies.access_token;
-  console.log(authHeader)
+  const token =req.cookies.access_token;
+  console.log(token)
   console.log("above header")
-  if (authHeader) {
+  if (token) {
     jwt.verify(token,process.env.SECRET, (err, user) => {
       if (err) {
         return res.status(403).json({msg:"you don't have permission please login first",status:false });

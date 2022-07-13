0b23d5de7890906269d5eefdab5e02f15fea5e22
unknown <beletemaruf@gmail.com>
2022-07-13 21:20:36 +0300

    correction for user profile

--- controllers/bus.controller.js
+++ controllers/bus.controller.js
@@ -74,7 +74,7 @@ exports.getDetailOrganizationBus = async (req, res, next) => {
     }
   },
   {
-    $project:{"_id":1,"busState":1,"onDuty":1,"busPlateNo":1,"busSideNo":1,"serviceYear":1,"totalNoOfSit":1,"driverName":{$arrayElemAt:["$driver.firstName",0]},"drverPhone":{$arrayElemAt:["$driver.phoneNumber",0]},"readtPhone":{$arrayElemAt:["$redat.phoneNumber",0]},}
+    $project:{"_id":1,"busState":1,"onDuty":1,"busPlateNo":1,"busSideNo":1,"serviceYear":1,"totalNoOfSit":1,"driverId":1,"drverPhone":{$arrayElemAt:["$driver.phoneNumber",0]},"readtPhone":{$arrayElemAt:["$redat.phoneNumber",0]},}
   },
 ])
   res.json(allbus)
